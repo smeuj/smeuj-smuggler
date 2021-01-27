8a0d38865a3931182d1953894f1f0450fde98071
Kumodatsu <dragonkumo@gmail.com>
2021-01-27 14:06:39 +0100

    Add Smeu record

---
+++ smeuj-smuggler/smeuj-smuggler/Smeu.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SmeujSmuggler {
+
+    internal record Smeu(
+        string   Author,
+        string?  Inspiration,
+        DateTime Time,
+        string   Content,
+        string?  Example
+     );
+
+}
