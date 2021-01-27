c7be82e1035992259eb08af2c206aa2d543d09c3
Kumodatsu <dragonkumo@gmail.com>
2021-01-27 13:31:05 +0100

    Parse command line arguments

---
+++ smeuj-smuggler/smeuj-smuggler/Options.cs
@@ -0,0 +1,15 @@
+﻿using CommandLine;
+
+namespace SmeujSmuggler {
+
+    internal class Options {
+        [Option('i', "identity", Required = false,
+            HelpText = "Specifies the path to the identity file.")]
+        public string? IdentityFilePath { get; set; }
+
+        [Option('s', "smeuj", Required = true,
+            HelpText = "Specifies the path to the file containing the smeuj.")]
+        public string? SmeujFilePath { get; set; }
+    }
+
+}
--- smeuj-smuggler/smeuj-smuggler/Program.cs
+++ smeuj-smuggler/smeuj-smuggler/Program.cs
@@ -1,3 +1,17 @@
 ﻿using System;
+using CommandLine;
+using Discord;
+using Discord.WebSocket;
 
-Console.WriteLine("Hello world!");
+using SmeujSmuggler;
+
+Parser.Default.ParseArguments<Options>(args)
+    .WithParsed(options => {
+        Console.WriteLine(options.IdentityFilePath);
+        Console.WriteLine(options.SmeujFilePath);
+    })
+    .WithNotParsed(errors => {
+        foreach (var error in errors) {
+            Console.WriteLine(error);
+        }
+    });
