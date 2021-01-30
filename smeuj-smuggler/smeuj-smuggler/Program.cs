b9538723344a8cf376cd41bd0d744269b88a7ee6
Kumodatsu <dragonkumo@gmail.com>
2021-01-31 00:19:38 +0100

    Log in bot and dump smeuj

--- smeuj-smuggler/smeuj-smuggler/Program.cs
+++ smeuj-smuggler/smeuj-smuggler/Program.cs
@@ -1,17 +1,31 @@
 ﻿using System;
+using System.Threading.Tasks;
 using CommandLine;
-using Discord;
-using Discord.WebSocket;
 
 using SmeujSmuggler;
 
+Options? options = null;
 Parser.Default.ParseArguments<Options>(args)
-    .WithParsed(options => {
-        Console.WriteLine(options.IdentityFilePath);
-        Console.WriteLine(options.SmeujFilePath);
-    })
+    .WithParsed(parsed_options => options = parsed_options)
     .WithNotParsed(errors => {
         foreach (var error in errors) {
             Console.WriteLine(error);
         }
     });
+
+if (options is null) return;
+
+var config = Deserialization.ReadConfig(options.IdentityFilePath!);
+if (config is null) {
+    Console.WriteLine("Could not read config file.");
+    return;
+}
+
+var smeuj  = Deserialization.ReadSmeuj(options.SmeujFilePath!);
+var client = new Client(config);
+
+client.OnLoggedIn += async () => await client.DumpSmeujAsync(smeuj);
+
+await client.LoginAsync();
+
+await Task.Delay(-1);
